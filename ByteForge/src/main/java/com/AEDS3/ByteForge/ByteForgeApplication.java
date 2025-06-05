5026041890a569c640d151ac766f22f42614f4d5
LuisHenriqueDC301 <luishenrique30102005@yahoo.com>
2025-06-05 09:39:33 -0300

    Initial Commit

---
+++ ByteForge/src/main/java/com/AEDS3/ByteForge/ByteForgeApplication.java
@@ -0,0 +1,13 @@
+package com.AEDS3.ByteForge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteForgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByteForgeApplication.class, args);
+	}
+
+}
---
+++ ByteForge/src/test/java/com/AEDS3/ByteForge/ByteForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AEDS3.ByteForge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByteForgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
